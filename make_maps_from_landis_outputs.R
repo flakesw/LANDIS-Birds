e8f86c88d9cd373a110b4051dbaefcb66460dab0
Sam Flake <swflake@ncsu.edu>
2024-04-02 10:09:18 -0400

    Update map script

--- make_maps_from_landis_outputs.R
+++ make_maps_from_landis_outputs.R
@@ -7,26 +7,35 @@ library("dismo")
 library("gbm")
 library("tidyverse")
 
+species <- "cerw"
+model_name <- "LowTLowV BAU"
+model_dir <- paste0("D:/SApps LANDIS/Model templates/", model_name, "/")
+input_dir <- "D:/SApps LANDIS/Inputs/"
+year <- 60
+
 ecoregions <- terra::rast("C:/Users/Sam/Documents/Research/Project-Southern-Appalachians-2018/Models/LANDIS_Sapps_Ecosystem_Papers/Ecos11_NCLD.tif")
 
 full_model <- readRDS("./models_for_landis/cerw_dist_model_full_landis.RDS")
 
 
-predictor_stack_0 <- terra::rast("landis_predictor_layers/pred_stack_LowTLowV BAU_0.tif")%>%
+predictor_stack_init <- terra::rast(paste0("./landis_predictor_layers/pred_stack_", 
+                                      model_name, "_", year, ".tif"))%>%
   terra::project(ecoregions) %>%
   mask(ecoregions, maskvalues = 1)
-predictor_stack_0$understory_ratio <- 1 - predictor_stack_0$understory_ratio
+predictor_stack$understory_ratio <- 1 - predictor_stack$understory_ratio
 
-predictor_stack_60 <- terra::rast("landis_predictor_layers/pred_stack_LowTLowV BAU_60.tif")%>%
+predictor_stack_init <- terra::rast(paste0("./landis_predictor_layers/pred_stack_", 
+                                           model_name, "_", 0, ".tif"))%>%
   terra::project(ecoregions) %>%
   mask(ecoregions, maskvalues = 1)
+predictor_stack_init$understory_ratio <- 1 - predictor_stack_init$understory_ratio
+# 
+# pred_stack_orig <- terra::rast("predictor_layers/predictor_stack.grd") %>%
+#   terra::project(ecoregions) %>%
+#   mask(ecoregions, maskvalues = 1)
+# # writeRaster(pred_stack_orig, "pred_stack_study_area.tif")
 
-pred_stack_orig <- terra::rast("predictor_layers/predictor_stack.grd") %>%
-  terra::project(ecoregions) %>%
-  mask(ecoregions, maskvalues = 1)
-# writeRaster(pred_stack_orig, "pred_stack_study_area.tif")
-
-preds <- terra::predict(object = predictor_stack_60, 
+preds <- terra::predict(object = predictor_stack, 
                         model = full_model,
                         # ext = st_bbox(bcr_albers),
                         const = data.frame(time_observations_started = 7,
@@ -51,11 +60,61 @@ ggplot() +
   # geom_sf(data = bcr_albers, fill = NA) +
   labs(fill = "P(observation)")
 
-writeRaster(preds*255, "study_area_preds_cerw.tif", overwrite = TRUE)
-#just climate variables
+writeRaster(preds, paste0("./landis_predictions/prediction_", species, "_", 
+                          model_name, "_", year, ".tif"), overwrite = TRUE)
+
+#--------------------
+
+#climate variables held constant
+clim_preds <- terra::predict(object = predictor_stack[[-c(4,5,6)]], 
+                                      model = full_model,
+                                      # ext = st_bbox(bcr_albers),
+                                      const = data.frame(time_observations_started = 7,
+                                                         duration_minutes = 60,
+                                                         tmmn = mean(predictor_stack_init$tmmn[]),
+                                                         tmmx = mean(predictor_stack_init$tmmx[]),
+                                                         pr = mean(predictor_stack_init$pr[]))
+        )
+
+values(clim_preds) <- boot::inv.logit(values(clim_preds)) 
+
+clim_preds <- clim_preds %>%
+  crop(ecoregions) %>%
+  terra::project(ecoregions) %>%
+  mask(ecoregions, maskvalues = 1)
+  
+ggplot() +
+  geom_sf(data = states, colour = "black", fill = NA) + 
+  geom_spatraster(data = clim_preds) +
+  scale_fill_terrain_c(alpha = 0.7) + 
+  # geom_sf(data = bcr_albers, fill = NA) +
+  labs(fill = "P(observation)")
 
 
+#----------------
+#vegetation variables held constant
 
+veg_preds <- terra::predict(object = predictor_stack[[-c(1,2,3)]], 
+                             model = full_model,
+                             # ext = st_bbox(bcr_albers),
+                             const = data.frame(time_observations_started = 7,
+                                                duration_minutes = 60,
+                                                understory_ratio = mean(predictor_stack_init$understory_ratio[]),
+                                                biomass = mean(predictor_stack_init$biomass[]),
+                                                prop_decid = mean(predictor_stack_init$prop_decid[]))
+)
 
-#just habitat variables
+values(veg_preds) <- boot::inv.logit(values(veg_preds)) 
+
+veg_preds <- veg_preds %>%
+  crop(ecoregions) %>%
+  terra::project(ecoregions) %>%
+  mask(ecoregions, maskvalues = 1)
+
+ggplot() +
+  geom_sf(data = states, colour = "black", fill = NA) + 
+  geom_spatraster(data = veg_preds) +
+  scale_fill_terrain_c(alpha = 0.7) + 
+  # geom_sf(data = bcr_albers, fill = NA) +
+  labs(fill = "P(observation)")
 
